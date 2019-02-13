07200383232398a26d33b61ebf2dd06851f694dc
Adrian Marten <adrian.marten@msg.group>
2019-02-13 09:30:21 +0100

    Fixed manifest information to javascript style, added Utils and models

--- scripts/popup/Component.js
+++ scripts/popup/Component.js
@@ -6,216 +6,211 @@ sap.ui.define([
     return UIComponent.extend('com.ui5.testing.Component', {
         metadata: {
             manifest: {
-                "_version": "1.0.0",
+                _version: "1.0.0",
                 "sap.app": {
-                    "_version": "1.0.0",
-                    "id": "com.ui5.testing",
-                    "type": "application",
-                    "i18n": "i18n/i18n.properties",
-                    "title": "{{appTitle}}",
-                    "description": "{{appDescription}}",
-                    "applicationVersion": {
-                        "version": "1.0.0"
+                    _version: "1.0.0",
+                    id: "com.ui5.testing",
+                    type: "application",
+                    i18n: "i18n/i18n.properties",
+                    title: "{{appTitle}}",
+                    description: "{{appDescription}}",
+                    applicationVersion: {
+                        version: "1.0.0"
                     },
-                    "dataSources": {
+                    dataSources: {
                     }
                 },
                 "sap.ui": {
-                    "_version": "1.3.0",
-                    "technology": "UI5",
-                    "icons": {
-                        "icon": "sap-icon://detail-view",
-                        "favIcon": "",
-                        "phone": "",
-                        "phone@2": "",
-                        "tablet": "",
-                        "tablet@2": ""
+                    _version: "1.3.0",
+                    technology: "UI5",
+                    icons: {
+                        icon: "sap-icon://detail-view"
                     },
-                    "deviceTypes": {
-                        "desktop": true,
-                        "tablet": true,
-                        "phone": true
+                    deviceTypes: {
+                        desktop: true,
+                        tablet: true,
+                        phone: true
                     },
-                    "supportedThemes": [
+                    supportedThemes: [
                         "sap_belize_plus"
                     ],
-                    "fullWidth": true
+                    fullWidth: true
                 },
                 "sap.ui5": {
-                    "_version": "1.2.0",
-                    "handleValidation": true,
-                    "rootView": {
-                        "viewName": "com.ui5.testing.view.App",
-                        "type": "XML",
-                        "id": "app"
+                    _version: "1.2.0",
+                    handleValidation: true,
+                    rootView: {
+                        viewName: "com.ui5.testing.view.App",
+                        type: "XML",
+                        id: "app"
                     },
-                    "dependencies": {
-                        "minUI5Version": "1.48.0",
-                        "libs": {
+                    dependencies: {
+                        minUI5Version: "1.48.0",
+                        libs: {
                             "sap.ui.core": {},
                             "sap.m": {},
                             "sap.ui.layout": {}
                         }
                     },
-                    "contentDensities": {
-                        "compact": true,
-                        "cozy": true
+                    contentDensities: {
+                        compact: true,
+                        cozy: true
                     },
-                    "models": {
-                        "i18n": {
-                            "type": "sap.ui.model.resource.ResourceModel",
-                            "settings": {
-                                "bundleName": "com.ui5.testing.i18n.i18n"
+                    models: {
+                        i18n: {
+                            type: "sap.ui.model.resource.ResourceModel",
+                            settings: {
+                                bundleName: "com.ui5.testing.i18n.i18n"
                             }
                         }
                     },
-                    "routing": {
-                        "config": {
-                            "routerClass": "sap.m.routing.Router",
-                            "viewType": "XML",
-                            "viewPath": "com.ui5.testing.view",
-                            "controlId": "idAppControl",
-                            "controlAggregation": "pages",
-                            "bypassed": {
-                                "target": [
+                    routing: {
+                        config: {
+                            routerClass: "sap.m.routing.Router",
+                            viewType: "XML",
+                            viewPath: "com.ui5.testing.view",
+                            controlId: "idAppControl",
+                            controlAggregation: "pages",
+                            bypassed: {
+                                target: [
                                     "ui5Testing"
                                 ]
                             },
-                            "async": true
+                            async: true
                         },
-                        "routes": [
+                        routes: [
                             {
-                                "pattern": "/testDetails/{TestId}/elementCreate/{ElementId}",
-                                "name": "elementCreate",
-                                "target": "elementCreate"
+                                pattern: "/testDetails/{TestId}/elementCreate/{ElementId}",
+                                name: "elementCreate",
+                                target: "elementCreate"
                             },
                             {
-                                "pattern": "/testDetails/{TestId}/elementCreateQuick/{ElementId}",
-                                "name": "elementCreateQuick",
-                                "target": "elementCreateQuick"
-                            }, 
-                            {
-                                "pattern": "/elementDisplay/{TestId}/{ElementId}",
-                                "name": "elementDisplay",
-                                "target": "elementDisplay"
+                                pattern: "/testDetails/{TestId}/elementCreateQuick/{ElementId}",
+                                name: "elementCreateQuick",
+                                target: "elementCreateQuick"
                             },
                             {
-                                "pattern": "/mockserver",
-                                "name": "mockserver",
-                                "target": "mockserver"
+                                pattern: "/elementDisplay/{TestId}/{ElementId}",
+                                name: "elementDisplay",
+                                target: "elementDisplay"
                             },
                             {
-                                "pattern": "/testDetails/{TestId}",
-                                "name": "testDetails",
-                                "target": "testDetails"
+                                pattern: "/mockserver",
+                                name: "mockserver",
+                                target: "mockserver"
                             },
                             {
-                                "pattern": "/testReplay/{TestId}",
-                                "name": "testReplay",
-                                "target": "testReplay"
+                                pattern: "/testDetails/{TestId}",
+                                name: "testDetails",
+                                target: "testDetails"
                             },
                             {
-                                "pattern": "/testDetailsCreate",
-                                "name": "testDetailsCreate",
-                                "target": "testDetailsCreate"
+                                pattern: "/testReplay/{TestId}",
+                                name: "testReplay",
+                                target: "testReplay"
                             },
                             {
-                                "pattern": "/testDetailsCreateQuick",
-                                "name": "testDetailsCreateQuick",
-                                "target": "testDetailsCreateQuick"
+                                pattern: "/testDetailsCreate",
+                                name: "testDetailsCreate",
+                                target: "testDetailsCreate"
                             },
                             {
-                                "pattern": "",
-                                "name": "start",
-                                "target": "start"
+                                pattern: "/testDetailsCreateQuick",
+                                name: "testDetailsCreateQuick",
+                                target: "testDetailsCreateQuick"
                             },
                             {
-                                "pattern": "/settings",
-                                "name": "settings",
-                                "target": "settings"
+                                pattern: "",
+                                name: "start",
+                                target: "start"
                             },
                             {
-                                "pattern": "/overview",
-                                "name": "overview",
-                                "target": "overview"
+                                pattern: "/settings",
+                                name: "settings",
+                                target: "settings"
+                            },
+                            {
+                                pattern: "/overview",
+                                name: "overview",
+                                target: "overview"
                             }
                         ],
-                        "targets": {
-                            "elementCreate": {
-                                "viewName": "ui5Testing",
-                                "viewLevel": 1,
-                                "viewId": "ui5Testing",
-                                "controlAggregation": "pages"
+                        targets: {
+                            elementCreate: {
+                                viewName: "ui5Testing",
+                                viewLevel: 1,
+                                viewId: "ui5Testing",
+                                controlAggregation: "pages"
                             },
-                            "elementCreateQuick": {
-                                "viewName": "ui5Testing",
-                                "viewLevel": 1,
-                                "viewId": "ui5Testing",
-                                "controlAggregation": "pages"
-                            }, 
-                            "mockserver": {
-                                "viewName": "mockserver",
-                                "viewLevel": 1,
-                                "viewId": "mockserver",
-                                "controlAggregation": "pages"
+                            elementCreateQuick: {
+                                viewName: "ui5Testing",
+                                viewLevel: 1,
+                                viewId: "ui5Testing",
+                                controlAggregation: "pages"
                             },
-                            "elementDisplay": {
-                                "viewName": "ui5Testing",
-                                "viewLevel": 1,
-                                "viewId": "ui5Testing",
-                                "controlAggregation": "pages"
+                            mockserver: {
+                                viewName: "mockserver",
+                                viewLevel: 1,
+                                viewId: "mockserver",
+                                controlAggregation: "pages"
                             },
-                            "testReplay": {
-                                "viewName": "testDetails",
-                                "viewLevel": 1,
-                                "viewId": "testDetails",
-                                "controlAggregation": "pages"
+                            elementDisplay: {
+                                viewName: "ui5Testing",
+                                viewLevel: 1,
+                                viewId: "ui5Testing",
+                                controlAggregation: "pages"
                             },
-                            "testDetails": {
-                                "viewName": "testDetails",
-                                "viewLevel": 1,
-                                "viewId": "testDetails",
-                                "controlAggregation": "pages"
+                            testReplay: {
+                                viewName: "testDetails",
+                                viewLevel: 1,
+                                viewId: "testDetails",
+                                controlAggregation: "pages"
                             },
-                            "testDetailsCreate": {
-                                "viewName": "testDetails",
-                                "viewLevel": 1,
-                                "viewId": "testDetails",
-                                "controlAggregation": "pages"
+                            testDetails: {
+                                viewName: "testDetails",
+                                viewLevel: 1,
+                                viewId: "testDetails",
+                                controlAggregation: "pages"
                             },
-                            "testDetailsCreateQuick": {
-                                "viewName": "testDetails",
-                                "viewLevel": 1,
-                                "viewId": "testDetails",
-                                "controlAggregation": "pages"
+                            testDetailsCreate: {
+                                viewName: "testDetails",
+                                viewLevel: 1,
+                                viewId: "testDetails",
+                                controlAggregation: "pages"
                             },
-                            "start": {
-                                "viewName": "start",
-                                "viewLevel": 1,
-                                "viewId": "start",
-                                "controlAggregation": "pages"
+                            testDetailsCreateQuick: {
+                                viewName: "testDetails",
+                                viewLevel: 1,
+                                viewId: "testDetails",
+                                controlAggregation: "pages"
                             },
-                            "overview": {
-                                "viewName": "testOverview",
-                                "viewLevel": 1,
-                                "viewId": "testOverview",
-                                "controlAggregation": "pages"
+                            start: {
+                                viewName: "start",
+                                viewLevel: 1,
+                                viewId: "start",
+                                controlAggregation: "pages"
                             },
-                            "settings": {
-                                "viewName": "settings",
-                                "viewLevel": 1,
-                                "viewId": "settings",
-                                "controlAggregation": "pages"
+                            overview: {
+                                viewName: "testOverview",
+                                viewLevel: 1,
+                                viewId: "testOverview",
+                                controlAggregation: "pages"
+                            },
+                            settings: {
+                                viewName: "settings",
+                                viewLevel: 1,
+                                viewId: "settings",
+                                controlAggregation: "pages"
                             }
                         }
                     },
-                    "resources": {
-                        "css": [
+                    resources: {
+                        css: [
                             {
-                                "uri": "css/style.css"
+                                uri: "css/style.css"
                             },
                             {
-                                "uri": "css/additional.css"
+                                uri: "css/additional.css"
                             }
                         ]
                     }
@@ -231,7 +226,7 @@ sap.ui.define([
         destroy: function () {
             UIComponent.prototype.destroy.apply(this, arguments);
         },
-        
+
         getContentDensityClass: function () {
             return 'sapUiSizeCompact';
         }
